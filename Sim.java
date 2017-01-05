735cec9a97e2e3359393aa4b2d562e904a0900fd
Jacqueline Xu <jackiestuy@gmail.com>
2017-01-04 22:06:05 -0500

    basic math and variables

--- Sim.java
+++ Sim.java
@@ -1,10 +1,17 @@
+import java.lang.Math;
+
 public class Sim {
     double mass;
     double height;
     double gravity;
     double coeff;
     double velocity;
+    double dist;
     double totalE;
+    double kineticE;
+    double potentialE;
+    double thermalE;
+    double angle;
 
     public Sim() {
 	mass = 50.0;
@@ -16,10 +23,6 @@ public class Sim {
 
     public void setHeight(double h) {
 	height = h;
-    }
-
-    public void setVelocity(double v) {
-	velocity = v;
     }
 
     public void setMass(double m) {
@@ -34,7 +37,29 @@ public class Sim {
 	coeff = c;
     }
 
-    public double calcTotalEnergy() {
-	return mass * gravity * height;
-    }	
+    public double setPotEnergy() {
+	double ans = mass * gravity * height;
+	totalE = ans;
+	return ans;
+    }
+
+    // need to set distance and angle
+    public double setThermEnergy() {
+	double ans = coeff * mass * gravity * Math.cos(angle) * dist;
+	thermalE = ans;
+	return ans;
+    }
+
+    public double calcVelocity() {
+	double ans = totalE - potentialE - thermalE;
+	ans = Math.sqrt((2 * ans)/mass);
+	velocity = ans;
+	return ans;
+    }
+
+    public double setKinEnergy() {
+	double ans = 0.5 * mass * velocity * velocity;
+	kineticE = ans;
+	return ans;
+    }
 }
